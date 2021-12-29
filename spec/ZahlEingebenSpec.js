03c9a3a66882697bef3412f9609f530c781f0d59
nicolas-00 <ni_ha@bluewin.ch>
2021-12-29 13:15:49 +0100

    UpdateSpecs

--- spec/ZahlEingebenSpec.js
+++ spec/ZahlEingebenSpec.js
@@ -1,3 +1,15 @@
+function testKey1Pressed(displayValue, newDisplayValue) {
+    var zahlEingeben = new ZahlEingeben();
+    zahlEingeben.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
+        "getDisplayValue": displayValue,
+        "setDisplayValue": undefined
+    }));
+    zahlEingeben.key1Pressed();
+    expect(zahlEingeben.calculatorContext.getDisplayValue).toHaveBeenCalled();
+    expect(zahlEingeben.calculatorContext.setDisplayValue).toHaveBeenCalledWith(newDisplayValue);
+}
+
+
 describe("ZahlEingeben", function() {
     it('should be able to be created', function() {
         var zahlEingeben = new ZahlEingeben();
@@ -6,30 +18,26 @@ describe("ZahlEingeben", function() {
 
     it('should be able to handle key1Pressed()', function() {
 
-        var zahlEingeben = new ZahlEingeben();
-        zahlEingeben.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
-            "getDisplayValue": 0,
-            "setDisplayValue": undefined
-        }));
-        zahlEingeben.key1Pressed();
-        expect(zahlEingeben.calculatorContext.getDisplayValue).toHaveBeenCalled();
-        expect(zahlEingeben.calculatorContext.setDisplayValue).toHaveBeenCalledWith(1);
+        testKey1Pressed(0, 1);
+        testKey1Pressed(11, 111);
+        testKey1Pressed(111111, 111111);
+        testKey1Pressed(2, 21)
 
-        var zahlEingeben = new ZahlEingeben();
-        zahlEingeben.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
-            "getDisplayValue": 1,
-            "setDisplayValue": undefined
-        }));
-        zahlEingeben.key1Pressed();
-        expect(zahlEingeben.calculatorContext.getDisplayValue).toHaveBeenCalled();
-        expect(zahlEingeben.calculatorContext.setDisplayValue).toHaveBeenCalledWith(11);
+        // var zahlEingeben = new ZahlEingeben();
+        // zahlEingeben.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
+        //     "getDisplayValue": 0,
+        //     "setDisplayValue": undefined
+        // }));
+        // zahlEingeben.key1Pressed();
+        // expect(zahlEingeben.calculatorContext.getDisplayValue).toHaveBeenCalled();
+        // expect(zahlEingeben.calculatorContext.setDisplayValue).toHaveBeenCalledWith(1);
 
-        zahlEingeben.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
-            "getDisplayValue": 111111,
-            "setDisplayValue": undefined
-        }));
-        zahlEingeben.key1Pressed();
-        expect(zahlEingeben.calculatorContext.getDisplayValue).toHaveBeenCalled();
-        expect(zahlEingeben.calculatorContext.setDisplayValue).toHaveBeenCalledWith(111111);
+        // zahlEingeben.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
+        //     "getDisplayValue": 111111,
+        //     "setDisplayValue": undefined
+        // }));
+        // zahlEingeben.key1Pressed();
+        // expect(zahlEingeben.calculatorContext.getDisplayValue).toHaveBeenCalled();
+        // expect(zahlEingeben.calculatorContext.setDisplayValue).toHaveBeenCalledWith(111111);
     });
 })
--- spec/ZahlLoeschenSpec.js
+++ spec/ZahlLoeschenSpec.js
@@ -1,3 +1,17 @@
+function testKeyCPressed(displayValue) {
+
+    var zahlLoeschen = new ZahlLoeschen();
+    zahlLoeschen.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
+        "setDisplayValue": 0
+    }));
+    zahlLoeschen.keyCPressed();
+    expect(zahlLoeschen.calculatorContext.setDisplayValue).toHaveBeenCalledWith(0);
+
+}
+
+
+
+
 describe("ZahlLoeschen", function() {
     it('should be able to be created', function() {
         var zahlLoeschen = new ZahlLoeschen();
@@ -6,27 +20,32 @@ describe("ZahlLoeschen", function() {
 
     it('should be able to handle keyCPressed()', function() {
 
-        var zahlLoeschen = new ZahlLoeschen();
-        zahlLoeschen.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
-            "getDisplayValue": 200,
-            "setDisplayValue": undefined
-        }));
-        zahlLoeschen.keyCPressed();
-        expect(zahlLoeschen.calculatorContext.setDisplayValue).toHaveBeenCalledWith(0);
+        testKeyCPressed(200);
+        testKeyCPressed(144);
+        testKeyCPressed(111111);
+        testKeyCPressed(34675);
 
-        var zahlLoeschen = new ZahlLoeschen();
-        zahlLoeschen.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
-            "getDisplayValue": 12,
-            "setDisplayValue": undefined
-        }));
-        zahlLoeschen.keyCPressed();
-        expect(zahlLoeschen.calculatorContext.setDisplayValue).toHaveBeenCalledWith(0);
+        // var zahlLoeschen = new ZahlLoeschen();
+        // zahlLoeschen.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
+        //     "getDisplayValue": 200,
+        //     "setDisplayValue": undefined
+        // }));
+        // zahlLoeschen.keyCPressed();
+        // expect(zahlLoeschen.calculatorContext.setDisplayValue).toHaveBeenCalledWith(0);
 
-        zahlLoeschen.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
-            "getDisplayValue": 123456,
-            "setDisplayValue": undefined
-        }));
-        zahlLoeschen.keyCPressed();
-        expect(zahlLoeschen.calculatorContext.setDisplayValue).toHaveBeenCalledWith(0);
+        // var zahlLoeschen = new ZahlLoeschen();
+        // zahlLoeschen.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
+        //     "getDisplayValue": 12,
+        //     "setDisplayValue": undefined
+        // }));
+        // zahlLoeschen.keyCPressed();
+        // expect(zahlLoeschen.calculatorContext.setDisplayValue).toHaveBeenCalledWith(0);
+
+        // zahlLoeschen.setCalculatorContext(jasmine.createSpyObj("calculatorContext", {
+        //     "getDisplayValue": 123456,
+        //     "setDisplayValue": undefined
+        // }));
+        // zahlLoeschen.keyCPressed();
+        // expect(zahlLoeschen.calculatorContext.setDisplayValue).toHaveBeenCalledWith(0);
     });
 })
